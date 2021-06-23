657c34e500ebce9793ece64dcbaca6e691c5d810
shreyaspanasa <55512246+shreyaspanasa@users.noreply.github.com>
2021-06-23 18:06:02 +0530

    Add files via upload

---
+++ shr.py
@@ -0,0 +1,38 @@
+import requests
+from datetime import datetime
+ 
+api_key = 8c531ea591fe563e95dc4ec4efe48015
+location = input("Enter the city name: ")
+ 
+comp_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+api_key
+api_link = requests.get(comp_api_link)
+api_data = api_link.json()
+ 
+ 
+temp_city = ((api_data['main']['temp']) - 273.15)
+weather_desc = api_data['weather'][0]['description']
+hmdt = api_data['main']['humidity']
+wind_spd = api_data['wind']['speed']
+date_time = datetime.now().strftime("%d %b %Y | %I:%M:%S %p")
+file = open("results.txt","w")
+ 
+file.write("\n")
+file.write ("Weather Stats for - {} || {}".format(location.upper(), date_time))
+ 
+file.write ("\n")
+ 
+file.write("Current temperature is: {:.2f} deg C".format(temp_city))
+ 
+file.write("\n")
+ 
+file.write("Current weather desc :"+ str(weather_desc))
+ 
+file.write("\n")
+ 
+file.write("Current Humidity :"+ str(hmdt) + "%")
+ 
+file.write("\n")
+ 
+file.write("Current wind speed :" + str(wind_spd) + "kmph")
+ 
+file.close ()
